cdd022ea9b745c12f5aed6adc1c12ce88feec88b
Edison Peñuela <edisonpe961206@hotmail.com>
2020-11-25 02:09:58 -0500

    Fixed list Items

--- src/Components/Placeholders/Card.jsx
+++ src/Components/Placeholders/Card.jsx
@@ -15,11 +15,13 @@ function CardPlaceholder() {
       <Card.Body>
         <Card.Title>Loading...</Card.Title>
         <Card.List>
-          <Loader>
-            <rect x="0" y="15" rx="5" ry="5" width="220" height="10" />
-            <rect x="0" y="45" rx="5" ry="5" width="220" height="10" />
-            <rect x="0" y="75" rx="5" ry="5" width="220" height="10" />
-          </Loader>
+          <Card.Item>
+            <Loader>
+              <rect x="0" y="15" rx="5" ry="5" width="220" height="10" />
+              <rect x="0" y="45" rx="5" ry="5" width="220" height="10" />
+              <rect x="0" y="75" rx="5" ry="5" width="220" height="10" />
+            </Loader>
+          </Card.Item>
         </Card.List>
       </Card.Body>
     </Card>
--- src/Components/Placeholders/Detail.jsx
+++ src/Components/Placeholders/Detail.jsx
@@ -25,28 +25,32 @@ function Detail() {
         <Country.Title>Loading...</Country.Title>
         <Country.Info>
           <Country.List>
-            <Loader>
-              <circle cx="10" cy="20" r="8" />
-              <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="50" r="8" />
-              <rect x="25" y="45" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="80" r="8" />
-              <rect x="25" y="75" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="110" r="8" />
-              <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
-            </Loader>
+            <li>
+              <Loader>
+                <circle cx="10" cy="20" r="8" />
+                <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="50" r="8" />
+                <rect x="25" y="45" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="80" r="8" />
+                <rect x="25" y="75" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="110" r="8" />
+                <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
+              </Loader>
+            </li>
           </Country.List>
           <Country.List>
-            <Loader>
-              <circle cx="10" cy="20" r="8" />
-              <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="50" r="8" />
-              <rect x="25" y="45" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="80" r="8" />
-              <rect x="25" y="75" rx="5" ry="5" width="220" height="10" />
-              <circle cx="10" cy="110" r="8" />
-              <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
-            </Loader>
+            <li>
+              <Loader>
+                <circle cx="10" cy="20" r="8" />
+                <rect x="25" y="15" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="50" r="8" />
+                <rect x="25" y="45" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="80" r="8" />
+                <rect x="25" y="75" rx="5" ry="5" width="220" height="10" />
+                <circle cx="10" cy="110" r="8" />
+                <rect x="25" y="105" rx="5" ry="5" width="220" height="10" />
+              </Loader>
+            </li>
           </Country.List>
         </Country.Info>
       </Country.Body>
