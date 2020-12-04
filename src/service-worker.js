93167d04430a69203c6e85e7551a1274c365020b
Edison Peñuela <edisonpe961206@hotmail.com>
2020-12-03 19:21:35 -0500

    add pwa resources and images to chache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,13 +1,8 @@
 /* eslint-disable no-restricted-globals */
 import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import {
-  StaleWhileRevalidate,
-  NetworkFirst,
-  CacheFirst,
-} from 'workbox-strategies';
+import { CacheFirst, NetworkFirst } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 // Clients.claim() hace que el trabajador del servicio tome el control de la página
@@ -23,37 +18,34 @@ precacheAndRoute(self.__WB_MANIFEST);
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false;
-    } // If this is a URL that starts with /_, skip.
-
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    } // Return true to signal that we want to use the handler.
-
-    return true;
-  },
+  ({ request, url }) =>
+    request.mode === 'navigate' &&
+    !url.pathname.startsWith('/_') &&
+    !url.pathname.match(fileExtensionRegexp),
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
-// An example runtime caching route for requests that aren't handled by the
-// precache, in this case same-origin .png requests like those from in public/
+// Cache images with a Cache First strategy
 registerRoute(
-  // Add in any other file extensions or routing criteria as needed.
-  ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
     cacheName: 'images',
     plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  new RegExp('/(manifest.json|service-worker.js)$'),
+  new NetworkFirst({
+    cacheName: 'pwa-resources',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
     ],
   })
 );
