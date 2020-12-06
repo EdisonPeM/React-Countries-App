307ee6477d42b13252813788051b0c8b1e731c64
Edison Peñuela <edisonpe961206@hotmail.com>
2020-12-05 23:15:20 -0500

    fix router Warnings

--- src/Components/RouterFade/index.jsx
+++ src/Components/RouterFade/index.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Location } from '@reach/router';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 import { className, duration, Router } from './styles';
 
-const RouterSlide = ({ children }) => (
-  <Location>
-    {({ location }) => (
-      <TransitionGroup component={null}>
-        <CSSTransition
-          key={location.key}
-          classNames={className}
-          timeout={duration}
-        >
-          <Router location={location}>{children}</Router>
-        </CSSTransition>
-      </TransitionGroup>
-    )}
-  </Location>
-);
+const RouterSlide = ({ children }) => {
+  const nodeRef = useRef(null);
 
+  return (
+    <Location>
+      {({ location }) => (
+        <TransitionGroup component={null}>
+          <CSSTransition
+            nodeRef={nodeRef}
+            key={location.key}
+            classNames={className}
+            timeout={duration}
+          >
+            <div ref={nodeRef}>
+              <Router location={location}>{children}</Router>
+            </div>
+          </CSSTransition>
+        </TransitionGroup>
+      )}
+    </Location>
+  );
+};
 export default RouterSlide;
